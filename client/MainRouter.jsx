e9b94ce201e0a94d7914f764022891192a6cd384
Doni Setiawan <donisetiawan.050@gmail.com>
2020-08-07 16:56:36 +0700

    ...

--- client/MainRouter.jsx
+++ client/MainRouter.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './core/Home';
 import Users from './user/Users';
@@ -9,32 +9,23 @@ import Profile from './user/Profile';
 import PrivateRoute from './auth/PrivateRoute';
 import Menu from './core/Menu';
 
-class MainRouter extends Component {
-  componentDidMount = () => {
-    const jssStyles = document.getElementById(
-      'jss-server-side',
-    );
-    if (jssStyles && jssStyles.parentNode) jssStyles.parentNode.removeChild(jssStyles);
-  };
-
-  render() {
-    return (
-      <div>
-        <Menu />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/users" component={Users} />
-          <Route path="/signup" component={Signup} />
-          <Route path="/signin" component={Signin} />
-          <PrivateRoute
-            path="/user/edit/:userId"
-            component={EditProfile}
-          />
-          <Route path="/user/:userId" component={Profile} />
-        </Switch>
-      </div>
-    );
-  }
+function MainRouter() {
+  return (
+    <div>
+      <Menu />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/users" component={Users} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/signin" component={Signin} />
+        <PrivateRoute
+          path="/user/edit/:userId"
+          component={EditProfile}
+        />
+        <Route path="/user/:userId" component={Profile} />
+      </Switch>
+    </div>
+  );
 }
 
 export default MainRouter;
